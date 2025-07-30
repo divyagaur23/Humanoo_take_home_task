eee874e1b034317164ea6049d698cbb5e7af2a34
divyagaur23 <36036833+divyagaur23@users.noreply.github.com>
2025-07-30 15:49:41 +0200

    Update user_clusters.py

--- user_clusters.py
+++ user_clusters.py
@@ -11,12 +11,14 @@ class KModeClustering:
         self.indices_cat = [0,1,3]
 
     def data_prep(self, df:pd.DataFrame)->pd.DataFrame:
+        """Data preprocessing for clustering"""
         df['user_goal'] = df['user_goal'].astype(str)
         df['content_preference'] = df['content_preference'].astype(str)
         df['gender'] = df['gender'].astype(str)
         return df
     
     def fit_model(self, df:pd.DataFrame)->pd.DataFrame:
+        """Groups users based on categorical (goal, preference, gender) and numerical (age) features"""
         df = self.data_prep(df)
         train_data = df[['user_goal','content_preference','age','gender']].copy()
         train_matrix = train_data.to_numpy()
@@ -27,12 +29,15 @@ class KModeClustering:
         return df
     
     def save_file(self, path="cluster_model.joblib") -> None:
+        """Save the resulting clustering mdoel for further use"""
         joblib.dump(self.model, path)
 
     def load_file(self, path = "cluster_model.joblib") ->None:
+        """load the saved model"""
         self.model = joblib.load(path)
 
     def predict_cluster(self, user_data:dict) ->int:
+        """Predict the cluster of the new user"""
         row = [
             str(user_data['user_goal']),
             str(user_data['content_preference']),
